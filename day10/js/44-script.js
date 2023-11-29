63ee2e1d36a5d7ee36335a420ed9a9fe25b24657
oner dogan <onerdogan91@gmail.com>
2023-11-30 00:17:25 +0300

    14

--- day10/js/44-script.js
+++ day10/js/44-script.js
@@ -11,6 +11,8 @@ function topla(a, b) {
   //!arrow func
   let yasHesapla = (dogumYili) => 2023 - dogumYili;
   console.log(yasHesapla(2000));
+  
+  
   const sum = (x, y) => {
     return x + y;
   };
@@ -19,4 +21,29 @@ function topla(a, b) {
     return str.length;
   };
   console.log(uzunluguBul("merhaba"));
+
+  const number = 10;
+
+const result = number < 0 ? -number : number;
+
+const message = result < 10 ? "The number is less than 10." : "The number is greater than or equal to 10.";
+
+console.log(message);
+
+
+  
+  let maas=parseInt(prompt("maaşınızı girirniz "));
+  
+ console.log(maas)
+  
+  let ikinciBasamak=0;
+  let üçüncüBasamak=0;
+  const promosyonHesapla=()=> {
+    let ilkBasamak=0;
+    ilkBasamak=maas>5000 ? "promosyon 5000":"promosyon 3000"
+   // ikinciBasamak=maas>10000 ? "promosyon 10000":"promosyon 5000"
+   // üçüncüBasamak=maas>20000 ? "promosyon 20000":"promosyon 10000"
+return ilkBasamak
+  }
+promosyonHesapla()
   
---
+++ day11/js/49.1-script.js
@@ -0,0 +1,10 @@
+
+(function(){
+    let name="Ali";
+    
+    const topla = (a,b) => a+b;
+    console.log(topla(3,5))
+    alert(name)
+    
+    })()
+    
